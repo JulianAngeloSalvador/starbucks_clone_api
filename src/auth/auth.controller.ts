4704890ca015edeb03d8caa3ba2b22d5a68d9372
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-22 17:55:32 +0800

    feat(/auth/): added signup, login routes + corresponding services

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserDto } from 'src/users/dto/create-user-dto';
+import { CredentialsDTO } from './dto/credentials-dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(@Body(ValidationPipe) user: UserDto) {
+    return this.authService.createUser(user);
+  }
+
+  @Post('login')
+  async login(@Body(ValidationPipe) user: CredentialsDTO) {
+    return this.authService.loginUser(user);
+  }
+}
