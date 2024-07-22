1692cc92f7bdfee59477beb443d27f9bb03530f7
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-22 17:59:24 +0800

    feat(/auth/module): imported User Model + added UsersService & JwtService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/users/schema/User.schema';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, UsersService, JwtService],
 })
 export class AuthModule {}
