769677abf6c9d87318a882581f1a1ccad81b0418
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-22 17:52:43 +0800

    feat(/users/): added User Model & basic func to fetch user by email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schema/User.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private Users: Model<User | User[]>) {}
+
+  // ---- Find by Email ---- //
+  async findByEmail(email: string) {
+    return await this.Users.findOne({ email });
+  }
+}
