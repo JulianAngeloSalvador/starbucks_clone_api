bc398496e81acb33b52a83d06aec584a659162ef
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-23 21:11:50 +0800

    feat(/users/serv): added service for fetching users with user role filtering by query params

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schema/User.schema';
+import { Roles, UserDto } from './dto/create-user-dto';
 
 @Injectable()
 export class UsersService {
@@ -10,5 +15,15 @@ export class UsersService {
   // ---- Find by Email ---- //
   async findByEmail(email: string) {
     return await this.Users.findOne({ email });
+  }
+
+  // ---- Find All ---- //
+
+  async findAll(role?: Roles): Promise<UserDto[]> {
+    if (role && !['ADMIN', 'USER'].includes(role))
+      throw new NotFoundException('Role not found');
+
+    const filter = role ? { role } : {};
+    return await this.Users.find(filter).select('-password');
   }
 }
