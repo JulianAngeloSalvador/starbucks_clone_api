f2e3de112a92ed49372e6b2f8e1100e8fbb3fc93
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-23 21:05:06 +0800

    feat(/users/): handled response for /users route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt-guard';
+import { Roles, UserDto } from './dto/create-user-dto';
+import { UsersService } from './users.service';
+import { RolesGuard } from 'src/auth/guards/role-guard';
+import { RolesAuthorized } from 'src/auth/decorators/roles.decorator';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtGuard, RolesGuard)
+  @RolesAuthorized('ADMIN')
+  @Get()
+  async findAll(@Query('role') role?: Roles): Promise<UserDto[]> {
+    return await this.usersService.findAll(role);
+  }
+}
