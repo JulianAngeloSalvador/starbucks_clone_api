fc1731ad6f21ced78ed308227093d5b1adfa2ddf
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-24 17:48:41 +0800

    feat(users/controller): added route for fetching a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guards/jwt-guard';
 import { Roles, UserDto } from './dto/create-user-dto';
 import { UsersService } from './users.service';
 import { RolesGuard } from 'src/auth/guards/role-guard';
 import { RolesAuthorized } from 'src/auth/decorators/roles.decorator';
+import mongoose from 'mongoose';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +15,11 @@ export class UsersController {
   @Get()
   async findAll(@Query('role') role?: Roles): Promise<UserDto[]> {
     return await this.usersService.findAll(role);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get(':id')
+  async findUser(@Param('id') id: string): Promise<UserDto> {
+    return await this.usersService.findUser(id);
   }
 }
