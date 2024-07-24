af1593bc3558b3c779084bb42affcf2f4d082b86
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-24 17:48:08 +0800

    feat(users/service): implemented user fetching service with role-based/id authorization

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,25 @@
 import {
   BadRequestException,
+  ExecutionContext,
+  Inject,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schema/User.schema';
 import { Roles, UserDto } from './dto/create-user-dto';
+import { validateObjectId } from 'src/lib/utils';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private Users: Model<User | User[]>) {}
+  constructor(
+    @InjectModel(User.name) private Users: Model<User | User[]>,
+    @Inject(REQUEST) private request: Request,
+  ) {}
 
   // ---- Find by Email ---- //
   async findByEmail(email: string) {
@@ -25,5 +34,27 @@ export class UsersService {
 
     const filter = role ? { role } : {};
     return await this.Users.find(filter).select('-password');
+  }
+
+  async findUser(requestedId: string): Promise<UserDto | null> {
+    const isValidId = await validateObjectId(requestedId);
+
+    if (!isValidId)
+      throw new BadRequestException('Please enter a valid user ID');
+
+    const userToFind =
+      await this.Users.findById(requestedId).select('-password');
+    const requester = this.request.user;
+
+    if (!userToFind) throw new NotFoundException('User not found');
+
+    if (requestedId !== requester.sub.id) {
+      if (requester.sub.role !== 'ADMIN')
+        throw new UnauthorizedException(
+          'Users info not accessible; Action not allowed',
+        );
+    }
+
+    return userToFind as UserDto;
   }
 }
